7c204b3f677f41b1706f7ef61125f4dba4bd1caa
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-10 20:15:30 +0100

    Feat: added Category component and changed font family

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainBanner from '../Components/MainBanner'
+import Categories from '../Components/Categories'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner/>
+        <Categories/>
     </div>
   )
 }
