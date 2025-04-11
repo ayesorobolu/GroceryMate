ef5e302741334d7d658f02acf31af5a9fee090f8
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-11 14:49:07 +0100

    Feat: added bestseller component
    Chore: updated context

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainBanner from '../Components/MainBanner'
 import Categories from '../Components/Categories'
+import BestSeller from '../Components/BestSeller'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner/>
         <Categories/>
+        <BestSeller/>
     </div>
   )
 }
