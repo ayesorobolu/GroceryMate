4757b47876d7bcddaf08b8e91d5b5714037e709f
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-14 15:31:06 +0100

    Feat: added login component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,14 +4,18 @@ import Home from './Pages/Home'
 import {Toaster} from "react-hot-toast"
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Footer from './Components/Footer'
+import { useAppContext } from './Context/AppContext'
+import Login from './Components/Login'
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext()
   return (
     <div>
       
       {isSellerPath ? null : <Navbar/> }
+      {showUserLogin ? <Login/> : null}
       
       <Toaster/>
       <div className= {` ${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
