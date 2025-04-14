1b5a27135d9498ef700ae495fa2e65d313c2f6bb
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-14 18:36:41 +0100

    Feat: added All products component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Footer from './Components/Footer'
 import { useAppContext } from './Context/AppContext'
 import Login from './Components/Login'
+import AllProducts from './Pages/AllProducts'
 
 const App = () => {
 
@@ -21,6 +22,7 @@ const App = () => {
       <div className= {` ${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={ <Home/>} />
+          <Route path='/products' element={ <AllProducts/>} />
         </Routes>
       </div>
      {! isSellerPath &&  <Footer/>}
--- Client/src/Context/AppContext.jsx
+++ Client/src/Context/AppContext.jsx
@@ -16,6 +16,7 @@ export const AppContextProvider = ({children}) => {
     const [products, setProducts] = useState([])
 
     const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setsearchQuery ] = useState({})
 
     //fetch all products
     const fetchProducts = async ()=>{
@@ -59,7 +60,8 @@ export const AppContextProvider = ({children}) => {
         fetchProducts()
     },[])
 
-    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem,removeFromCart,cartItems}
+    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart,
+     updateCartItem,removeFromCart,cartItems, searchQuery, setsearchQuery}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
