ea1e616cd43d934c0205e118462509820aaf174f
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-15 16:00:19 +0100

    Feat: added cart icon update function

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useAppContext } from '../Context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const{user,setUser,setShowUserLogin,navigate,setSearchQuery, searchQuery} = useAppContext();
+    const{user,setUser,setShowUserLogin,navigate,setSearchQuery, searchQuery, getCartCount} = useAppContext();
 
     const logout = async () => {
         setUser(null);
@@ -37,7 +37,7 @@ const Navbar = () => {
 
         <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
             <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
-            <button className="absolute -top-2 -right-3 text-xs text-white bg-[var(--color-primary)] w-[18px] h-[18px] rounded-full">3</button>
+            <button className="absolute -top-2 -right-3 text-xs text-white bg-[var(--color-primary)] w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
         </div>
 
         { !user ?   (
@@ -55,10 +55,19 @@ const Navbar = () => {
         )}
     </div>
 
-    <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+    <div className='flex items-center gap-6 sm-hidden'>
+    <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+            <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
+            <button className="absolute -top-2 -right-3 text-xs text-white bg-[var(--color-primary)] w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+        </div>
+
+        <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" >
         {/* Menu Icon SVG */}
        <img src={assets.menu_icon} alt="menu" />
     </button>
+    </div>
+
+
 
     {/* Mobile Menu */}
     { open && (
--- Client/src/Pages/ProductDetails.jsx
+++ Client/src/Pages/ProductDetails.jsx
@@ -30,7 +30,7 @@ const ProductDetails = () => {
             <p>
                 <Link to={"/"}>Home</Link> /
                 <Link to={"/products"}> Products</Link> /
-                <Link to={`products/${product.category.toLowerCase()}`}> {product.category}</Link> /
+                <Link to={`/products/${product.category.toLowerCase()}`}> {product.category}</Link> /
                 <span className="text-[var(--color-primary)]"> {product.name}</span>
             </p>
 
