90625ec5c8fdfec681b148854b57bf8cab732f4a
ayesorobolu <abrahamayesoro@gmail.com>
2025-04-28 22:56:58 +0100

    feat: added user registration route

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config.js'; 
+import userRouter from './routes/userRoute.js';
+
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -17,6 +19,7 @@ app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials:true}));
 
 app.get("/", (req,res) => res.send("API Working") );
+app.use("/api/user", userRouter);
 
 app.listen (port,() => {
 console.log(`server running on ${port}`)
