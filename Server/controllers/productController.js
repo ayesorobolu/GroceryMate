4185669d85cc327527ed46f04ba99c3dd11b48b5
ayesorobolu <abrahamayesoro@gmail.com>
2025-04-29 17:57:25 +0100

    feat: added productController with  addProduct functionality

---
+++ Server/controllers/productController.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import Product from '../models/Product';
+
+//Add product : /api/products/add
+export const addProduct = async (req, res) => {
+ try {
+    let productData = JSON.parse(req.body.productData)
+
+    const images = req.files
+
+    let imagesUrl = await Promise.all(
+        images.map(async (item) => {
+            let result = await cloudinary.uploader.upload(item.path,
+                {resource_type : "image"})
+            return result.secure_url
+        })
+    )
+
+    await Product.create({...productData, image: imagesUrl})
+
+    res.json({success:true, message:"Product Added"})
+ } catch (error) {
+    console.log(error)
+    res.json({success:false, message:error.message})
+ }
+}
