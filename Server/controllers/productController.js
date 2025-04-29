13367e021487abe1349ea7cfa22f7f63a7c271e3
ayesorobolu <abrahamayesoro@gmail.com>
2025-04-29 18:19:04 +0100

    Feat: added change product in stock functionality

--- Server/controllers/productController.js
+++ Server/controllers/productController.js
@@ -47,3 +47,15 @@ export const productById = async (req,res) => {
         res.json({success:false, message:error.message})
     }
 }
+
+//Change Product inStock : /api/product/stock
+export const changeStock = async (req,res) => {
+    try {
+        const { id, inStock } = req.body
+        await Product.findByIdAndUpdate(id, {inStock})
+        res.json({success:true, message:" Stock Updated"})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false, message:error.message})
+    }
+}
