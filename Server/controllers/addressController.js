27a49def860cb030f7cf3da134763affa2715299
ayesorobolu <abrahamayesoro@gmail.com>
2025-04-29 19:10:33 +0100

    Feat:Created address controller and added add address function

---
+++ Server/controllers/addressController.js
@@ -0,0 +1,13 @@
+import Address from "../models/Address";
+
+//Add addres : /api/address/add
+export const addAddress = async (req, res) => {
+    try {
+        const {address, userId} = req.body;
+        await Address.create({...address, userId});
+        res.json({success: true, message: "Address added successfully"});
+    } catch (error) {
+        cpnsole.log(error.message)
+        res.json({success: false, message: error.message});
+    }
+}
