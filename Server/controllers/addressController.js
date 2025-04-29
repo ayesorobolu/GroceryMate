6e35496aaf76b9be39e6791e0f47779d84a0b2ea
ayesorobolu <abrahamayesoro@gmail.com>
2025-04-29 19:14:26 +0100

    Feat: added get address function

--- Server/controllers/addressController.js
+++ Server/controllers/addressController.js
@@ -7,7 +7,19 @@ export const addAddress = async (req, res) => {
         await Address.create({...address, userId});
         res.json({success: true, message: "Address added successfully"});
     } catch (error) {
-        cpnsole.log(error.message)
+        console.log(error.message)
+        res.json({success: false, message: error.message});
+    }
+}
+
+//Get address : /api/address/get
+export const getAddress = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const addresses = await Address.find({ userId });
+        res.json({success: true, addresses});
+    } catch (error) {
+        console.log(error.message)
         res.json({success: false, message: error.message});
     }
 }
