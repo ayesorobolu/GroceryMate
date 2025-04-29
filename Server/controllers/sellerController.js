433ace0ec90fd826ab6552d3ea44fae6057f3718
ayesorobolu <abrahamayesoro@gmail.com>
2025-04-29 16:48:26 +0100

    feat: added seller login functionality and added authSeller middleware for seller authentication

---
+++ Server/controllers/sellerController.js
@@ -0,0 +1,25 @@
+
+//Login seller: /api/seller/login
+
+export const sellerLogin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+    if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL){
+        const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.cookie('sellerToken', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        return res.json({success:true, message:"Login successful"})
+    }else{
+        return res.json({success:false, message:"Invalid email or password"})
+    }
+    } catch (error) {
+        console.error(error.message);
+        res.json({success:false, message: error.message})
+    }
+}
+
