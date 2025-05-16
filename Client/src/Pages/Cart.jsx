72df7fe26b6030352dfddfc28fce7dad6946b6d5
ayesorobolu <abrahamayesoro@gmail.com>
2025-05-16 19:45:33 +0100

    Feat: added a function to fetch and manage user delivery addresses in the cart page

--- Client/src/Pages/Cart.jsx
+++ Client/src/Pages/Cart.jsx
@@ -1,14 +1,15 @@
 import React, { use, useEffect, useState } from 'react'
 import { assets, dummyAddress } from '../assets/assets'
 import { useAppContext } from '../Context/AppContext';
+import toast from 'react-hot-toast';
 
 const Cart = () => {
     
-    const {products, navigate, currency, cartItems, removeFromCart, getCartCount, updateCartItem, getCartAmount} = useAppContext();
+    const {products, navigate, currency, cartItems, removeFromCart, getCartCount, updateCartItem, getCartAmount, axios, user} = useAppContext();
     const[cartArray, setCartArray] = useState([])
-    const[addresses, setAddresses] = useState(dummyAddress)
+    const[addresses, setAddresses] = useState([])
     const [showAddress, setShowAddress] = useState(false)
-    const [selectedAddress, setSelectedAddress] = useState(dummyAddress[0]);
+    const [selectedAddress, setSelectedAddress] = useState(null);
     const[paymentOption, setPaymentOption] = useState("COD")
 
     const getCart = () =>{
@@ -21,6 +22,22 @@ const Cart = () => {
         setCartArray(tempArray)
     }
 
+    const getUserAddress = async () => {
+        try {
+            const {data} = await axios.get("/api/address/get");
+            if (data.success){
+                setAddresses(data.addresses)
+                if(data.addresses.length > 0){
+                    setSelectedAddress(data.addresses[0])
+                }
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const placeOrder = async () => {
 
     }
@@ -30,6 +47,12 @@ const Cart = () => {
             getCart()
         }
     },[products, cartItems])
+
+    useEffect (() => {
+        if (user){
+            getUserAddress()
+        }
+    },[user])
 
     return products.length > 0 && cartItems ? (
         <div className="flex flex-col md:flex-row mt-16">
